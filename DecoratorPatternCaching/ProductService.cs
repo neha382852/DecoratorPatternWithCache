d95eeb7da859d058d2d2fc7fd50a7935bd0526fa
Neha Anand <nanand@tavisca.com>
2018-09-11 14:55:53 +0530

    Implementation of cache with decorator pattern

---
+++ DecoratorPatternCaching/ProductService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorPatternCaching
+{
+    class ProductService : IProductService
+    {
+        public List<Product> GetAllProducts()
+        {
+            IRepository productRepository = new SQLRepository();
+            return productRepository.GetAllProducts();
+        }
+
+        public Product GetProductById(int id)
+        {
+            IRepository productRepo = new SQLRepository();
+            Console.WriteLine("From Repository");
+            return productRepo.GetProductById(id);
+        }
+    }
+}
---
+++ DecoratorPatternCaching/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorPatternCaching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IProductService productService = new CacheProductService();
+            List<Product> products = productService.GetAllProducts();
+            Console.WriteLine("All Products");
+            for(int i=0;i<products.Count;i++)
+            {
+                Console.WriteLine("ProductId:- " + products[i].Id);
+                Console.WriteLine("ProductName:- " + products[i].name);
+                Console.WriteLine("ProductPrice:- " + products[i].price);
+                Console.WriteLine();
+            }
+            Console.WriteLine("Enter productId which u want to search");
+            int id = int.Parse(Console.ReadLine());
+            Product product = productService.GetProductById(id);
+            Console.WriteLine("ProductId:- " + product.Id);
+            Console.WriteLine("ProductName:- " + product.name);
+            Console.WriteLine("ProductPrice:- " + product.price);
+            Console.WriteLine("Enter productId which u want to search");
+            Console.WriteLine();
+            id = int.Parse(Console.ReadLine());
+            product = productService.GetProductById(id);
+            Console.WriteLine("ProductId:- "+product.Id);
+            Console.WriteLine("ProductName:- " + product.name);
+            Console.WriteLine("ProductPrice:- " + product.price);
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
